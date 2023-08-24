fb102e2c41a0c8c57211ee77d9a7ce8862215641
popoxta <popoxta@gmail.com>
2023-08-25 09:55:44 +1200

    Create test stubs

---
+++ tests/api-client.test.ts
@@ -0,0 +1,23 @@
+import {expect, it, describe} from 'vitest'
+
+describe('getCards', () => {
+  it('Should return an array of raw data cards')
+})
+
+describe('getAllCards', () => {
+  it('Should return all mapped cards')
+})
+
+describe('getCardsBySuit', () => {
+  it('Should return mapped cards by suit')
+})
+
+describe('getMajorCards', () => {
+  it('Should return all mapped major cards')
+})
+
+describe('getCardByValue', () => {
+  it('Should return a single card by value')
+  it('Should return the card mapped to contain the full dataset')
+  it('Should throw if the queried card does not exist')
+})
