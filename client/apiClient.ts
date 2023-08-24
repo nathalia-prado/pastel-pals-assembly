2196570569bf53abbfbe533bdaa80d526d4d546d
popoxta <popoxta@gmail.com>
2023-08-24 19:01:52 +1200

    Add superagent

---
+++ client/apiClient.ts
@@ -0,0 +1,14 @@
+import request from 'superagent'
+
+const TAROT_URL = 'https://tarot-api-3hv5.onrender.com/api/v1/'
+
+
+function getAllCards() {
+
+}
+
+// API CLIENT METHOD
+//
+// GET all tarot cards under specified WANDS/SWORDS/COINS/CUPS etc.
+//
+//   e.g getTarotCards('wands')
