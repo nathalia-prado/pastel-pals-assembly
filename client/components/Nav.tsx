f1d2cfe1e0f14dc806c5fb8df40ff27fafac8544
Frances Krsinich <fkrsinich@yahoo.co.nz>
2023-08-25 11:02:12 +1200

    add the routing

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
     <div>
       <h2>Nav</h2>
       <ul>
-        <li><Link to={`cards/test`}>TEST</Link> </li>
+        <li>
+          <Link to={`cards/:type`}>TEST</Link>{' '}
+        </li>
       </ul>
     </div>
   )
 }
 
 export default Nav
-
---
+++ client/components/Type.tsx
@@ -0,0 +1,9 @@
+function Type() {
+  return (
+    <>
+      <h2>Hello world again!</h2>
+    </>
+  )
+}
+
+export default Type
