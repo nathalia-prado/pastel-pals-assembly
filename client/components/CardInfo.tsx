5f99b98869b64929d48d8f33d8dd1ac857b3a283
Nathalia Prado <nathalia_898@hotmail.com>
2023-08-25 12:31:21 +1200

    Add CardInfo component

---
+++ client/components/CardInfo.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { FullCard as FullCardType } from "../../models/types";
+import { getCardByValue } from "../apiClient";
+import { useParams } from 'react-router-dom';
+
+export default function Card() {
+    const [card, setCard] = useState<FullCardType | undefined>(undefined)
+    const params = useParams();
+
+    useEffect(() => {
+        async function fetchCard() {
+            const cardData = await getCardByValue(params.id || '')
+            console.log(cardData)
+            setCard(cardData)
+        }
+
+        try {
+            fetchCard()
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
+
+    // prevent show invalid cards
+    if (!card) {
+        return null
+    }
+
+    return (
+        <div>
+            <h2><b>{card.name}</b></h2>
+            <p><b>Type: </b>{card.type}</p>
+            <p><b>Meaning Up: </b>{card.meaningUp}</p>
+            <p><b>Meaning Reverse: </b>{card.meaningReverse}</p>
+            <p><b>Description: </b>{card.description}</p>
+        </div>
+    )
+}
